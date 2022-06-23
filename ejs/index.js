8d657c7408efa31bacf25044bdaec4d6a03d462e
Juan Manuel Van Oyen <JMRodriguezVanOyen@teco.com.ar>
2022-06-23 19:49:15 -0300

    ejs & fix names

--- ejs/index.js
+++ ejs/index.js
@@ -5,20 +5,11 @@ const apiRouter = require('./src/RouterApi');
 const path = require('path');
 const PORT = 8080;
 const menuItems = require('./src/modules/menu');
-const handlebars = require('express-handlebars')
-const {engine} = handlebars;
 
-app.engine(
-    "hbs",
-    engine({
-        extname: ".hbs",
-        defaultLayout: "layout.hbs",
-    })
-);
 app.locals.navItems = menuItems.menu;
 
 app.set("views", "./src/views");
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 
 app.use('/', apiRouter.router);
 app.use('/', apiRouter.errorHandler);
--- ejs/src/RouterApi.js
+++ ejs/src/RouterApi.js
@@ -3,7 +3,6 @@ const path = require('path');
 const router = express.Router();
 
 const Controlador = require('./Controller');
-const mainDir = path.normalize(__dirname+"/..");
 
 const errorHandler = (err, req, res, next)=>{
     res.status(err.status || 500);
@@ -18,10 +17,9 @@ router.get('/',(req, res)=>{
     res.render("home", {
         pageTitle: "Entregable Nº 5",
         rutaDestino: "./productos",
-        scripts : [{ script: './home.js' }]
+        scripts : [{ script: './home.js' }],
+        isFormUploaded: false
     });
-    //res.sendFile( mainDir + '/public/index.html');
-    //res.send(Controlador.getProductos());
 });
 router.get('/form-success',(req, res)=>{
     const params = {
@@ -39,8 +37,6 @@ router.post('/productos',(req, res)=>{
     }else{
         res.redirect('./');
     }
-    //res.sendFile( mainDir + '/public/productos.html');
-    //res.send(Controlador.getProductos());
 });
 router.get('/productos',(req, res)=>{
     let prods = Controlador.getProductos();
@@ -48,34 +44,12 @@ router.get('/productos',(req, res)=>{
        prods = false; 
     }
     const params = {
-        pageTitle: "Entregable Nº 5",
-        productos : prods
+        pageTitle: "Entregable Nº 5 | Ejs",
+        productos : prods,
+        scripts : false
     };
     res.render("productos", params);
 });
-
-// router.post('/',(req, res)=>{
-//     const prod = Controlador.saveProducto(req.body);
-//     (prod) ? res.status(200).json(prod) : res.status(400).json({error: `Error en los parametros requeridos`});
-// })
-
-// router.get('/:id',(req, res)=>{
-//     const { id } = req.params;
-//     const prod = Controlador.getProductoById(id);
-//     (prod) ? res.status(200).json(prod) : res.status(404).json({error: `Producto no encontrado`});   
-// })
-
-// router.put('/:id',(req, res)=>{
-//     const { id } = req.params;
-//     const prod = Controlador.putProductoById(id, req.body);
-//     (prod) ? res.status(200).json(prod) : res.status(400).json({error: `Error al actualizar`});   
-// })
-
-// router.delete('/:id',(req, res)=>{
-//     const { id } = req.params;
-//     const action = Controlador.delProductoById(id);
-//     (action) ? res.status(200).json() : res.status(404).json();
-// })
 
 module.exports = {
     router,
